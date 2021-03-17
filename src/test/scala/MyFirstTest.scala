1ea09b6caa4141f68d545762d9a933e4a72c7f7b
Amu <am.martinez.domingo@gmail.com>
2021-03-17 09:55:32 +0100

    adding proxy (I haven't set it up in local, see section 18 from training)

--- src/test/scala/MyFirstTest.scala
+++ src/test/scala/MyFirstTest.scala
@@ -9,6 +9,7 @@ class MyFirstTest extends Simulation{
   //1 Http conf
   val httpConf = http.baseUrl("http://localhost:8080/app/")
     .header("Accept","application/json")
+    //.proxy(Proxy("localhost",8888))
 
   //2 Scenario definition
   val scn = scenario("My First test")
